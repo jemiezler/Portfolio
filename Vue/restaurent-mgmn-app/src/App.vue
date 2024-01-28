349d108863fbbf16ad1daffe53af7ee836ec2f16
jemiezler <142890376+jemiezler@users.noreply.github.com>
2024-01-29 02:46:27 +0700

    rma-init signup
    Restaurent init
    Get signup page

---
+++ Vue/restaurent-mgmn-app/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import signup from './components/signup.vue';
+export default{
+  name: 'App',
+  components:{
+    signup
+  }
+}
+</script>
+
+<template>
+  <signup />
+</template>
+
+
+<style >
+
+</style>
---
+++ Vue/restaurent-mgmn-app/src/components/signup.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="content">
+        <img class="logo" src="../assets/logo.ico">
+        <h1>Sign Up</h1>
+        <div class="register">
+            <div class="half">
+                <input type="text" placeholder="Name">
+                <input type="text" placeholder="Family-Name"> 
+            </div>
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <input type="checkbox"> I agree to the Terms & Conditions
+            <button type="submit">Create my account</button>
+        </div>
+    </div>
+
+    <!--- background part --->
+    <div class="gradient-bg">
+        <div class="gradients-container">
+        <div class="g1"></div>
+        <div class="g2"></div>
+        <div class="g3"></div>
+        <div class="g4"></div>
+        <div class="g5"></div>
+        <div class="interactive"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'signup',
+    mounted() {
+        const interBubble = this.$el.querySelector('.interactive');
+        let curX = 0;
+        let curY = 0;
+        let tgX = 0;
+        let tgY = 0;
+
+        function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(move);
+        }
+
+        window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+        });
+
+        move();
+    }
+}
+</script>
+<style>
+
+.logo{
+    width: 100px;
+}
+    
+</style>
